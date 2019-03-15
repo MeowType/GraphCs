4ae486725872685973ec957b49c90e67da7cd036
2A5F <Aditrc@outlook.com>
2019-03-16 00:21:37 +0800

    ValueGraph

--- Graph/IGraph.cs
+++ Graph/IGraph.cs
@@ -6,23 +6,47 @@ namespace MeowType.Collections.Graph
     public interface IGraphGet<T, E> where E : IEnumerable<T>
     {
         E this[T index] { get; }
-        bool TryGetTo(T key, out E values);
+        bool TryGetTo(T item, out E connections);
     }
 
-    public interface IDigraphGet<T, E> : IGraphGet<T, E> where E : IEnumerable<T>
+    public interface IDigraphGet<T, E> where E : IEnumerable<T>
     {
-        bool TryGetFrom(T value, out E keys);
+        bool TryGetFrom(T to, out E froms);
     }
 
-    public interface IGraph<T> : ICollection<T>, IEnumerable<T>, IEnumerable
+    public interface IValueGraphGet<T, V, E> where E : IEnumerable<V>
     {
-        bool Has(T key);
-        bool Has(T key, T value);
+        E this[T from, T to] { get; }
+        bool TryGetValues(T from, T to, out E values);
     }
 
-    public interface IDigraph<T> : IGraph<T>
+    public interface IValueGraphSet<T, V>
     {
-        void Set(T a, T b);
-        bool UnSet(T a, T b);
+        void Set(T item1, T item2, V value);
     }
+
+    public interface IValueGraphSetIndex<T, V> : IValueGraphSet<T, V>
+    {
+        V this[T from, T to] { set; }
+    }
+
+    public interface IReadOnlyGraph<T> : ICollection<T>, IEnumerable<T>, IEnumerable, IReadOnlyCollection<T>
+    {
+        bool Has(T item);
+        bool Has(T item1, T item2);
+    }
+
+    public interface IGraph<T> : IReadOnlyGraph<T>
+    {
+        void Set(T item1, T item2);
+        bool UnSet(T item1, T item2);
+    }
+
+    public interface IValueGraph<T, V> : IReadOnlyGraph<T>, IValueGraphSet<T, V>
+    {
+        bool Has(T item1, T item2, V value);
+        bool UnSet(T item1, T item2);
+        bool UnSet(T item1, T item2, V value);
+    }
+
 }
